b202eaa76787fdbf51dd8b46e8d68644240e6ccf
Alexander Velez <fabildsur@gmail.com>
2021-04-29 17:16:42 -0500

    update async api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Controllers
 
@@ -22,16 +24,16 @@ namespace Controllers
        }
 
        [HttpGet]
-       public ActionResult<IEnumerable<AppUser>> GetUsers()
+       public  async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
        {
-             return _context.Users.ToList();
+             return await _context.Users.ToListAsync();
              
        }
 
          [HttpGet("{id}")]
-       public ActionResult<AppUser> GetUsers(int id)
+       public async Task<ActionResult<AppUser>> GetUsers(int id)
        {
-           return _context.Users.Find(id);
+           return await _context.Users.FindAsync(id);
              
        }
     }
