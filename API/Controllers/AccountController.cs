c5fe714c0602fe4bfae2fb4c89597cd0c4dcf421
Alexander Velez <fabildsur@gmail.com>
2021-05-03 07:27:29 -0500

    Agregamos Login

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -31,8 +32,8 @@ namespace API.Controllers
 
             var user = new AppUser
             {
-                UserName = registerDTO.username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.password)),
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -43,6 +44,26 @@ namespace API.Controllers
 
         }
 
+        
+        [HttpPost("login")]
+
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(users =>users.UserName == loginDTO.Username );
+            
+            if(user == null) return Unauthorized("Usuario Invalido");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var ComputeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for(int i = 0;i < ComputeHash.Length;i++)
+            {
+                if(ComputeHash[i] != user.PasswordHash[i]) return Unauthorized("Password invalido");
+            }
+
+            return user;
+
+        }
         private Task<bool> UserExists(object username)
         {
             throw new NotImplementedException();
---
+++ API/DTOs/LoginDTO.cs
@@ -0,0 +1,13 @@
+namespace API.DTOs
+{
+
+    public class LoginDTO
+    {
+        internal readonly string Username;
+
+        public string Userrname { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
